a2c564534ca106d68c274a4d3be77c4bdabcce9c
federico-code <federicodelaurentiis7@gmail.com>
2021-01-30 12:40:43 +0100

    adding creation file method in WI

--- src/tekno/WI.java
+++ src/tekno/WI.java
@@ -4,6 +4,11 @@ package tekno;
 
 import org.jsoup.Jsoup;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 import org.json.*;
 
@@ -30,7 +35,33 @@ public class WI {
     }
     
 
+    public static void getText(String entity, String folder){
+    	String text="";
+       	Writer writer = null;
+
+    	try {
+
+    		String json = Jsoup.connect(base_URL+entity.replace(" ", "%20")).ignoreContentType(true).execute().body();
+    		JSONObject obj = (new JSONObject(json)).getJSONObject("query").getJSONObject("pages");
+    		
+    		text = (obj.getJSONObject(obj.keySet().toArray()[0].toString()).getString("extract").toString());
+    	
     
+    	
+	    	System.out.println("writin file: "+ entity);
+	    	File f = new File (folder,entity+".txt");
+	    	f.createNewFile();
+	    	System.out.println("path file: "+ f.getAbsolutePath());
+	    	writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f), "utf-8"));
+	    	writer.write(text);
+	    	} catch (Exception e) {
+				System.err.println(e.getMessage());
+			} finally {
+				   try {writer.close();} catch (Exception ex) {/*ignore*/}
+			    	System.out.println("file"+entity +".txt close ");
+	
+			}
+    }
 
     
     
