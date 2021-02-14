3ad6d6fd50309e3b3676bdaa9945637b635b4394
federico-code <federicodelaurentiis7@gmail.com>
2021-02-14 16:40:35 +0100

    adding test class with old main

---
+++ src/tekno/Test.java
@@ -0,0 +1,21 @@
+  
+package tekno;
+
+public class Test {
+    
+    public static void main(String[] args) throws Exception {
+    	
+    	KnowledgeGraph kg = new KnowledgeGraph("bolt://localhost:7687","neo4j","pass");
+    	HighLevelParsing hlp = new HighLevelParsing();
+    	if(kg.resetGraph()) {
+    		hlp.readFile("./source_files/bg.txt");
+        	hlp.executeTeKnoPipeline();
+        	hlp.generateGraphDB(kg);
+    	}
+    	
+    	kg.extractFacts("bg", "prolog_files");
+    	kg.close();
+    	
+    }
+
+}
