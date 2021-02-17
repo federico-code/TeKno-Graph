7e243d543dbffed3c25f99414e1b3f386eadb3d4
federico-code <federicodelaurentiis7@gmail.com>
2021-02-17 18:28:50 +0100

    prolog rules for lit facts

---
+++ prolog_files/bg_complete.pl
@@ -0,0 +1,171 @@
+per_title(172657602,85957749).
+literal_of(172657602,'melinda william henry gates iii').
+literal_of(85957749,'chairman').
+per_date_of_birth(172657602,218468342).
+literal_of(218468342,'october 28 , 1955').
+per_alternate_names(172657602,164761519).
+literal_of(164761519,'bill gates').
+per_title(172657602,213048309).
+literal_of(213048309,'architect').
+per_schools_attended(172657602,236552086).
+literal_of(236552086,'microsoft corporation').
+per_statesorprovinces_of_residence(172657602,218109813).
+literal_of(218109813,'new mexico').
+per_date_of_birth(172657602,168037076).
+literal_of(168037076,'january 2000').
+per_spouse(172657602,142440908).
+literal_of(142440908,'william henry gates iii').
+per_schools_attended(172657602,216623144).
+literal_of(216623144,'bill & melinda gates foundation').
+per_title(172657602,231762403).
+literal_of(231762403,'chief executive officer').
+per_employee_or_member_of(172657602,236552086).
+per_title(172657602,155180480).
+literal_of(155180480,'president').
+per_cities_of_residence(172657602,3478450).
+literal_of(3478450,'albuquerque').
+per_date_of_birth(172657602,248703426).
+literal_of(248703426,'june 2006').
+per_spouse(172657602,172657602).
+per_title(236642198,66605).
+literal_of(236642198,'jeff bezos').
+literal_of(66605,'ceo').
+per_title(236642198,130665391).
+literal_of(130665391,'founder').
+per_employee_or_member_of(236642198,85520932).
+literal_of(85520932,'amazon').
+org_top_members_employees(236552086,68574440).
+literal_of(68574440,'gates').
+org_stateorprovince_of_headquarters(236552086,218109813).
+org_city_of_headquarters(236552086,3478450).
+org_date_founded(236552086,1537214).
+literal_of(1537214,2000).
+org_alternate_names(236552086,216623144).
+org_date_founded(236552086,218468342).
+org_top_members_employees(236552086,9618427).
+literal_of(9618427,'satya nadella').
+org_date_dissolved(236552086,218468342).
+alias(236552086,30691534).
+literal_of(30691534,'microsoft').
+org_founded_by(236552086,172657602).
+org_top_members_employees(236552086,142440908).
+org_top_members_employees(236552086,172657602).
+org_top_members_employees(236552086,164761519).
+is_a(236552086,6542643).
+literal_of(6542643,'organization').
+is_a(205535187,79833656).
+literal_of(205535187,'philanthropist').
+literal_of(79833656,'title').
+is_a(166844770,2090926).
+literal_of(166844770,'october 28, 1955').
+literal_of(2090926,'date').
+is_a(172735196,2090926).
+literal_of(172735196,'may 2014').
+alias(30691534,236552086).
+is_a(30691534,6542643).
+is_a(47006035,2090926).
+literal_of(47006035,'1980s').
+org_stateorprovince_of_headquarters(67880,2470).
+literal_of(67880,'dos').
+literal_of(2470,'ms').
+alias(1537214,168037076).
+is_a(66605,79833656).
+alias(66605,231762403).
+per_spouse(142440908,104673685).
+literal_of(104673685,'ray ozzie').
+per_title(142440908,66605).
+per_title(142440908,23871287).
+literal_of(23871287,'magnate').
+is_a(142440908,209096533).
+literal_of(209096533,'person').
+per_stateorprovince_of_birth(142440908,66994608).
+literal_of(66994608,'washington').
+per_title(142440908,85957749).
+per_origin(142440908,151308352).
+literal_of(151308352,'american').
+per_title(142440908,205535187).
+per_date_of_birth(142440908,218468342).
+per_title(142440908,187754442).
+literal_of(187754442,'developer').
+per_date_of_birth(142440908,168037076).
+per_employee_or_member_of(142440908,236552086).
+per_title(142440908,208768846).
+literal_of(208768846,'co-founder').
+alias(142440908,164761519).
+is_a(231762403,79833656).
+alias(231762403,66605).
+org_country_of_headquarters(225605060,114966431).
+literal_of(225605060,'microsoft microsoft corporation').
+literal_of(114966431,'united states').
+org_city_of_headquarters(225605060,63757001).
+literal_of(63757001,'redmond').
+org_stateorprovince_of_headquarters(225605060,66994608).
+org_founded_by(225605060,142440908).
+org_parents(225605060,75447618).
+literal_of(75447618,'nokia').
+org_subsidiaries(225605060,14137325).
+literal_of(14137325,'danger inc.').
+org_parents(14137325,75447618).
+org_shareholders(14137325,225605060).
+org_date_founded(14137325,1537222).
+literal_of(1537222,2008).
+org_top_members_employees(85520932,236642198).
+org_date_founded(75447618,248703453).
+literal_of(248703453,'june 2012').
+is_a(208768846,79833656).
+org_date_founded(216623144,168037076).
+org_top_members_employees(216623144,164761519).
+org_date_founded(216623144,218468342).
+org_date_dissolved(216623144,218468342).
+org_top_members_employees(216623144,172657602).
+org_alternate_names(216623144,236552086).
+is_a(151308352,98324892).
+literal_of(98324892,'nationality').
+alias(114966431,33572806).
+literal_of(33572806,'united states of america').
+alias(33572806,114966431).
+is_a(213048309,79833656).
+per_title(54140910,231762403).
+literal_of(54140910,'steve ballmer').
+is_a(23871287,79833656).
+is_a(47005074,2090926).
+literal_of(47005074,'1970s').
+org_country_of_headquarters(143120610,114966431).
+literal_of(143120610,'united united states of america of america').
+is_a(110182,166448969).
+literal_of(110182,'one').
+literal_of(166448969,'number').
+per_alternate_names(9618427,164761519).
+per_title(9618427,66605).
+per_employee_or_member_of(9618427,236552086).
+is_a(85957749,79833656).
+is_a(187754442,79833656).
+per_employee_or_member_of(164761519,216623144).
+per_date_of_birth(164761519,1537214).
+per_city_of_birth(164761519,143397032).
+literal_of(143397032,'seattle').
+per_spouse(164761519,172657602).
+alias(164761519,142440908).
+is_a(164761519,209096533).
+per_title(164761519,85957749).
+per_title(164761519,23871287).
+per_title(164761519,213048309).
+per_schools_attended(164761519,216623144).
+per_employee_or_member_of(164761519,236552086).
+per_date_of_birth(164761519,218468342).
+per_title(164761519,66605).
+per_alternate_names(164761519,9618427).
+per_alternate_names(164761519,172657602).
+per_title(68574440,231762403).
+per_employee_or_member_of(68574440,236552086).
+per_title(68574440,213048309).
+per_title(68574440,66605).
+per_title(68574440,85957749).
+per_title(68574440,155180480).
+is_a(68574440,209096533).
+is_a(155180480,79833656).
+per_cities_of_residence(172863538,3478450).
+literal_of(172863538,'paul allen').
+per_statesorprovinces_of_residence(172863538,218109813).
+per_employee_or_member_of(172863538,236552086).
+:- ensure_loaded('\\dynamics_facts_rules.pl').
