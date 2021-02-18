954317d90d301835d2397bd179e727f6853a7179
federico-code <federicodelaurentiis7@gmail.com>
2021-02-18 17:48:18 +0100

    adding rules, bg file ultimate

--- prolog_files/dynamics_facts_rules.pl
+++ prolog_files/dynamics_facts_rules.pl
@@ -1,102 +1,124 @@
-:-dynamic	gpe_subsidiaries/2.
-:-dynamic	org_alternate_names/2.
-:-dynamic	org_city_of_headquarters/2.
-:-dynamic	org_country_of_headquarters/2.
-:-dynamic	org_date_dissolved/2.
-:-dynamic	org_date_founded/2.
-:-dynamic	org_dissolved/2.
-:-dynamic	org_founded/2.
-:-dynamic	org_founded_by/2.
-:-dynamic	org_member_of/2.
-:-dynamic	org_members/2.
-:-dynamic	org_number_of_members/2. 
-:-dynamic	org_number_of_employees/2.
-:-dynamic	org_parents/2.
-:-dynamic	org_religious_affiliation/2.
-:-dynamic	org_political/2.
-:-dynamic	org_shareholders/2.
-:-dynamic	org_stateorprovince_of_headquarters/2.
-:-dynamic	org_subsidiaries/2.
-:-dynamic	org_top_members/2.
-:-dynamic	org_top_employees/2.
-:-dynamic	org_website/2.
-:-dynamic	per_age/2.
-:-dynamic	per_alternate_names/2.
-:-dynamic	per_cause_of_death/2.
-:-dynamic	per_charges/2.
-:-dynamic	per_children/2.
-:-dynamic	per_cities_of_residence/2.
-:-dynamic	per_city_of_birth/2.
-:-dynamic	per_city_of_death/2.
-:-dynamic	per_countries_of_residence/2.
-:-dynamic	per_country_of_birth/2.
-:-dynamic	per_country_of_death/2.
-:-dynamic	per_date_of_birth/2.
-:-dynamic	per_date_of_death/2.
-:-dynamic	per_employee_of/2.
-:-dynamic	per_member_of/2.
-:-dynamic	per_origin/2.
-:-dynamic	per_other_family/2.
-:-dynamic	per_parents/2.
-:-dynamic	per_religion/2.
-:-dynamic	per_schools_attended/2.
-:-dynamic	per_siblings/2.
-:-dynamic	per_spouse/2.
-:-dynamic	per_stateorprovince_of_birth/2.
-:-dynamic	per_stateorprovince_of_death/2.
-:-dynamic	per_stateorprovinces_of_residence/2.
-:-dynamic	per_title/2.
+:-dynamic	subsidiaries/2.
+:-dynamic	alternate_names/2.
+:-dynamic	city_of_headquarters/2.
+:-dynamic	country_of_headquarters/2.
+:-dynamic	date_dissolved/2.
+:-dynamic	date_founded/2.
+:-dynamic	dissolved/2.
+:-dynamic	founded/2.
+:-dynamic	founded_by/2.
+:-dynamic	member_of/2.
+:-dynamic	members/2.
+:-dynamic	number_of_members/2. 
+:-dynamic	number_of_employees/2.
+:-dynamic	parents/2.
+:-dynamic	religious_affiliation/2.
+:-dynamic	political/2.
+:-dynamic	shareholders/2.
+:-dynamic	stateorprovince_of_headquarters/2.
+:-dynamic	subsidiaries/2.
+:-dynamic	top_members/2.
+:-dynamic	top_employees/2.
+:-dynamic	website/2.
+:-dynamic	age/2.
+:-dynamic	alternate_names/2.
+:-dynamic	cause_of_death/2.
+:-dynamic	charges/2.
+:-dynamic	children/2.
+:-dynamic	cities_of_residence/2.
+:-dynamic	city_of_birth/2.
+:-dynamic	city_of_death/2.
+:-dynamic	countries_of_residence/2.
+:-dynamic	country_of_birth/2.
+:-dynamic	country_of_death/2.
+:-dynamic	date_of_birth/2.
+:-dynamic	date_of_death/2.
+:-dynamic	employee_or_member_of/2.
+:-dynamic	origin/2.
+:-dynamic	other_family/2.
+:-dynamic	parents/2.
+:-dynamic	religion/2.
+:-dynamic	schools_attended/2.
+:-dynamic	siblings/2.
+:-dynamic	spouse/2.
+:-dynamic	stateorprovince_of_birth/2.
+:-dynamic	stateorprovince_of_death/2.
+:-dynamic	stateorprovinces_of_residence/2.
+:-dynamic	title/2.
+
+:-dynamic   person/1.
+:-dynamic	islocation/1.
+:-dynamic	organization/1.
+:-dynamic	misc/1.
+:-dynamic	money/1.
+%:-dynamic	number/1.
+:-dynamic	ordinal/1.
+:-dynamic	percent/1.
+:-dynamic	date/1.
+%:-dynamic	time.
+:-dynamic   duration/1.
+:-dynamic	set/1.
+:-dynamic	email/1.
+:-dynamic	url/1.
+:-dynamic	city/1.
+:-dynamic	state_of_province/1.
+:-dynamic	country/1.
+:-dynamic	nationality/1.
+:-dynamic	religion/1.
+:-dynamic	title/1.
+:-dynamic	ideology/1.
+:-dynamic	criminal_charge/1.
+:-dynamic	cause_of_death/1.
 
 
-%utility
-	print_person:- person(X),literal_of(X,Z),write(Z),nl,fail.
-	print_person.
 
-	my_print(X,TEXT,Y) :- format('~w ~s ~w ~n', [X,TEXT,Y]). 
+% utility
+		my_print(X,TEXT,Y) :- format('~w ~s ~w ~n', [X,TEXT,Y]).
 
-	%if an element has an per_... fact he is a person so we assert the fact is_a person
-	find_person:- per_title(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
+		alias_of(LIT_X,LIT_Y) :- literal_of(X,LIT_X),literal_of(Y,LIT_Y),alias(X,Y).
 
-	find_person:- per_date_of_birth(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
+		print_person:- person(X),literal_of(X,Z),write(Z),nl,fail.
 
-	find_person:- per_cause_of_death(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	
-	find_person:- per_title(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-
-	find_person:- per_age(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_children(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_cities_of_residence(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_city_of_birth(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_city_of_death(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_countries_of_residence(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_country_of_birth(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_country_of_death(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_date_of_death(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_employee_of(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_member_of(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_origin(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_other_family(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_parents(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_religion(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_schools_attended(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_siblings(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_spouse(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_stateorprovince_of_birth(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_stateorprovince_of_death(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person:- per_stateorprovinces_of_residence(X,_),\+person(X), literal_of(LIT_PER,'person'),write(X),assert(is_a(X,LIT_PER)) ,nl,fail.
-	find_person.
+		print_org:- organization(X),literal_of(X,Z),write(Z),nl,fail.
 
 
-	person(X):- is_a(X,Y),literal_of(Y,'person').
-	show_alias(LIT_X,LIT_Y) :- alias(X,Y),literal_of(X,LIT_X),literal_of(Y,LIT_Y),my_print(LIT_X,' alias of',LIT_Y).
-
-%title of a person
-%title_of_person(PERSON) :-person(X),per_title(X,Y),literal_of(X,PERSON),literal_of(Y,TITLE),my_print(PERSON,' has the title of',TITLE).
-
-title_of_person(PERSON) :-person(X),
-							alias(X,Z),
-							per_title(Z,Y),
-							literal_of(Z,PERSON),
+% title of a person 
+title_of_person(PER) :-literal_of(X,PER),
+							person(X),
+							title(X,Y),
 							literal_of(Y,TITLE),
-							my_print(PERSON,' has the title of',TITLE).
+							my_print(PER,' has the title of',TITLE).
 
+% person of an organization
+organization_employee(ORG) :-literal_of(ID_ORG,ORG),
+								organization(ID_ORG),
+								employee_or_member_of(ID_PER,ID_ORG),
+								literal_of(ID_PER,PER),
+								my_print(PER,'work for',ORG).
+
+% information abouth the foundetion of an organization
+founded(ORG) :- literal_of(ID_ORG,ORG),
+					organization(ID_ORG),
+					date_founded(ID_ORG,ID_DATE),
+					literal_of(ID_DATE,DATE),
+					my_print(ORG,'was founded on',DATE).
+
+founded(ORG) :- literal_of(ID_ORG,ORG),
+					organization(ID_ORG),
+					founded_by(ID_ORG,ID_FOUNDER),
+					literal_of(ID_FOUNDER,FOUNDER),
+					my_print(ORG,'was founded by',FOUNDER).
+
+% when born 
+when_born(PER):-literal_of(ID_PER,PER),
+			person(ID_PER),
+			date_of_birth(ID_PER,ID_DATE),
+			literal_of(ID_DATE,DATE),
+			my_print(PER,'was born on',DATE).
+
+% where born, si può fare lo stesso per  country_of_birth stateorprovince_of_birth
+where_born(PER):-literal_of(ID_PER,PER),
+			person(ID_PER),
+			city_of_birth(ID_PER,ID_CITY),
+			literal_of(ID_CITY,CITY),
+			my_print(PER,'was born in',CITY).
