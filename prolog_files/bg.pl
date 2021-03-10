43667fedf33fb643a6cc801b50b756ed4db7d34c
federico-code <federicodelaurentiis7@gmail.com>
2021-03-10 10:38:21 +0100

    rules with recursion and list in file recursive_rules

--- prolog_files/bg.pl
+++ prolog_files/bg.pl
@@ -1,137 +1,131 @@
-date_founded(216623144,1537214).
-organization(216623144).
-literal_of(216623144,'bill & melinda gates foundation').
-literal_of(1537214,2000).
-top_members_employees(216623144,140845150).
-person(140845150).
-literal_of(140845150,'melinda gates').
-alias(114966431,33572806).
-literal_of(114966431,'united states').
-literal_of(33572806,'united states of america').
-alias(33572806,114966431).
-title(236642198,231762403).
-person(236642198).
-title(231762403).
-literal_of(236642198,'jeff bezos').
-literal_of(231762403,'chief executive officer').
-employee_or_member_of(236642198,85520932).
-organization(85520932).
-literal_of(85520932,'amazon').
-title(236642198,130665391).
-literal_of(130665391,'founder').
-top_members_employees(236552086,68574440).
-organization(236552086).
-person(68574440).
-literal_of(236552086,'microsoft corporation').
-literal_of(68574440,'gates').
-city_of_headquarters(236552086,3478450).
-literal_of(3478450,'albuquerque').
-stateorprovince_of_headquarters(236552086,218109813).
-literal_of(218109813,'new mexico').
-top_members_employees(236552086,140845150).
-alternate_names(236552086,216623144).
-alias(236552086,30691534).
-organization(30691534).
-literal_of(30691534,'microsoft').
-top_members_employees(236552086,9618427).
-person(9618427).
-literal_of(9618427,'satya nadella').
-title(54140910,66605).
-person(54140910).
-title(66605).
-literal_of(54140910,'steve ballmer').
-literal_of(66605,'ceo').
-alias(30691534,236552086).
-top_members_employees(30691534,140845150).
-alternate_names(143120610,151308357).
-organization(143120610).
-literal_of(143120610,'united united states of america of america').
-literal_of(151308357,'americas').
-country_of_headquarters(143120610,114966431).
-stateorprovince_of_headquarters(67880,2470).
-organization(67880).
-literal_of(67880,'dos').
-literal_of(2470,'ms').
-employee_or_member_of(9618427,236552086).
-title(9618427,231762403).
-alias(66605,231762403).
-title(142440908,205535187).
-person(142440908).
-title(205535187).
-literal_of(142440908,'william henry gates iii').
-literal_of(205535187,'philanthropist').
-title(142440908,204284299).
-literal_of(204284299,'author').
-title(142440908,23871287).
-title(23871287).
-literal_of(23871287,'magnate').
-origin(142440908,151308352).
-nationality(151308352).
-literal_of(151308352,'american').
-title(142440908,187754442).
-title(187754442).
-literal_of(187754442,'developer').
-date_of_birth(142440908,218468342).
-literal_of(218468342,'october 28 , 1955').
-alias(142440908,164761519).
-person(164761519).
-literal_of(164761519,'bill gates').
-alias(231762403,66605).
-stateorprovince_of_headquarters(225605060,66994608).
-organization(225605060).
-literal_of(225605060,'microsoft microsoft corporation').
-literal_of(66994608,'washington').
-subsidiaries(225605060,14137325).
-organization(14137325).
-literal_of(14137325,'danger inc.').
-parents(225605060,75447618).
-organization(75447618).
-literal_of(75447618,'nokia').
-country_of_headquarters(225605060,114966431).
-founded_by(225605060,142440908).
-city_of_headquarters(225605060,63757001).
-literal_of(63757001,'redmond').
-shareholders(14137325,225605060).
-parents(14137325,75447618).
-date_founded(14137325,1537222).
-literal_of(1537222,2008).
-cities_of_residence(140845150,3478450).
-title(140845150,208768846).
-title(208768846).
-literal_of(208768846,'co-founder').
-employee_or_member_of(140845150,216623144).
-spouse(140845150,140845150).
-title(140845150,66605).
-title(140845150,231762403).
-title(140845150,213048309).
-title(213048309).
-literal_of(213048309,'architect').
-statesorprovinces_of_residence(140845150,218109813).
-city_of_birth(140845150,143397032).
-literal_of(143397032,'seattle').
-stateorprovince_of_birth(140845150,66994608).
-title(140845150,85957749).
-title(85957749).
-literal_of(85957749,'chairman').
-title(140845150,155180480).
-title(155180480).
-literal_of(155180480,'president').
-employee_or_member_of(140845150,236552086).
-other_family(140845150,140845150).
-top_members_employees(85520932,236642198).
-title(164761519,205535187).
-title(164761519,23871287).
-alias(164761519,142440908).
-date_founded(75447618,248703453).
-literal_of(248703453,'june 2012').
-title(68574440,213048309).
-title(68574440,155180480).
-title(68574440,85957749).
-employee_or_member_of(68574440,236552086).
-title(68574440,66605).
-cities_of_residence(172863538,3478450).
-person(172863538).
-literal_of(172863538,'paul allen').
-statesorprovinces_of_residence(172863538,218109813).
-employee_or_member_of(172863538,236552086).
-:- ensure_loaded('\\dynamics_facts_rules.pl').
+date_founded(43,66).
+organization(43).
+name(43,'bill & melinda gates foundation').
+name(66,2000).
+top_members_employees(43,76).
+person(76).
+name(76,'melinda gates').
+alias(45,47).
+name(45,'united states').
+name(47,'united states of america').
+alias(47,45).
+title(50,71).
+person(50).
+title(71).
+name(50,'jeff bezos').
+name(71,'chief executive officer').
+employee_or_member_of(50,77).
+organization(77).
+name(77,'amazon').
+title(50,63).
+name(63,'founder').
+alternate_names(52,43).
+organization(52).
+name(52,'microsoft corporation').
+stateorprovince_of_headquarters(52,72).
+name(72,'new mexico').
+top_members_employees(52,65).
+person(65).
+name(65,'satya nadella').
+top_members_employees(52,76).
+top_members_employees(52,80).
+person(80).
+name(80,'gates').
+title(56,71).
+person(56).
+name(56,'steve ballmer').
+alias(57,52).
+name(57,'microsoft').
+organization(57).
+alternate_names(60,42).
+organization(60).
+name(60,'united united states of america of america').
+name(42,'americas').
+country_of_headquarters(60,45).
+stateorprovince_of_headquarters(64,61).
+organization(64).
+name(64,'dos').
+name(61,'ms').
+employee_or_member_of(65,52).
+title(65,71).
+alias(68,71).
+title(68).
+name(68,'ceo').
+title(69,58).
+person(69).
+title(58).
+name(69,'william henry gates iii').
+name(58,'magnate').
+alias(69,78).
+person(78).
+name(78,'bill gates').
+title(69,55).
+title(55).
+name(55,'philanthropist').
+origin(69,44).
+nationality(44).
+name(44,'american').
+date_of_birth(69,70).
+name(70,'october 28 , 1955').
+title(69,62).
+name(62,'author').
+title(69,74).
+title(74).
+name(74,'developer').
+alias(71,68).
+stateorprovince_of_headquarters(73,51).
+organization(73).
+name(73,'microsoft microsoft corporation').
+name(51,'washington').
+subsidiaries(73,75).
+organization(75).
+name(75,'danger inc.').
+parents(73,79).
+organization(79).
+name(79,'nokia').
+country_of_headquarters(73,45).
+founded_by(73,69).
+city_of_headquarters(73,46).
+name(46,'redmond').
+shareholders(75,73).
+parents(75,79).
+date_founded(75,54).
+name(54,2008).
+title(76,81).
+title(81).
+name(81,'president').
+spouse(76,76).
+statesorprovinces_of_residence(76,72).
+cities_of_residence(76,49).
+name(49,'albuquerque').
+city_of_birth(76,59).
+name(59,'seattle').
+title(76,71).
+title(76,67).
+title(67).
+name(67,'chairman').
+title(76,68).
+other_family(76,76).
+title(76,82).
+title(82).
+name(82,'co-founder').
+stateorprovince_of_birth(76,51).
+employee_or_member_of(76,52).
+title(76,48).
+title(48).
+name(48,'architect').
+employee_or_member_of(76,43).
+top_members_employees(77,50).
+title(78,55).
+alias(78,69).
+date_founded(79,53).
+name(53,'june 2012').
+title(80,68).
+title(80,81).
+title(80,67).
+employee_or_member_of(80,52).
+title(80,48).
+cities_of_residence(83,49).
+person(83).
+name(83,'paul allen').
+statesorprovinces_of_residence(83,72).
--- src/tekno/Test.java
+++ src/tekno/Test.java
@@ -5,15 +5,15 @@ public class Test {
     
     public static void main(String[] args) throws Exception {
     	
-    	KnowledgeGraph kg = new KnowledgeGraph("bolt://localhost:7687","neo4j","pass");
+    	KnowledgeGraph kg = new KnowledgeGraph("bolt://localhost:7687","neo4j","corenlp");
     	HighLevelParsing hlp = new HighLevelParsing();
     	if(kg.resetGraph()) {
     		hlp.readFile("./source_files/bg.txt");
-        	hlp.executeTeKnoPipeline(false);
+        	hlp.executeTeKnoPipeline();
         	hlp.generateGraphDB(kg);
     	}
     	
-    	kg.extractFacts("prova", "prolog_files");
+    	kg.extractFacts("bg", "prolog_files");
     	kg.close();
     	
     }
